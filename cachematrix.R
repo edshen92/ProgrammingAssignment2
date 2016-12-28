9801bc38326a65cd42d82f85e8be46c6082c522c
edshen92 <eddie.shen@uq.net.au>
2016-12-29 03:53:19 +1000

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## Compute the inversion of a square matrix 
+##and cache it so that it need not be repeatedly computed
+
+## Create a matrix object which can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m = NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## Computes the inverse of the matrix returned by the 
+##above function
+##Retrive inverse from cache if already calculated
+
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+        ## Return a matrix that is the inverse of 'x'
+}
